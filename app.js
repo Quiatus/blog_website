d9dc3c7874e5fe5b951095a8c3b339e33f503f34
unknown <quiatus@gmail.com>
2022-10-04 13:52:14 +0200

    adding commentiong

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+var posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -17,19 +18,28 @@ app.use(express.static("public"));
 
 app.get('/',function(req,res){
   res.render('home',{homeCont: homeStartingContent});
-})
+});
 
 app.get('/about',function(req,res){
   res.render('about',{aboutCont: aboutContent});
-})
+});
 
 app.get('/contact',function(req,res){
   res.render('contact',{contactCont: contactContent});
-})
+});
 
 app.get('/compose',function(req,res){
   res.render('compose');
-})
+});
+
+app.post('/compose',function(req,res){
+  let post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect('/');
+});
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
